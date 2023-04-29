a237acf6552b74a4d7f0f84f78d4d5208e7b6d09
Wade Sedgwick <wsedgwick0@gmail.com>
2023-04-29 10:06:16 -0700

    worked on shiny app, able to get rasters loaded into leaflet but they need to be subsetted first, need to figure out how to get entire CA raster to show

--- shiny/dump.R
+++ shiny/dump.R
@@ -13,14 +13,40 @@ sidebarPanel(width = 3,
 # ABOVE AFTER LEAFLET MAP
 
 
-r <- raster("/Users/Wsedgwick/Desktop/bren_meds/courses/capstone/aquafire/shiny/www/tslf.tif")
+tslf <- raster("/Users/Wsedgwick/Desktop/bren_meds/courses/capstone/aquafire/shiny/www/tslf.tif")
+gde <- raster("/Users/Wsedgwick/Desktop/bren_meds/courses/capstone/aquafire/shiny/www/gde_boundaries.tif")
 
-raster::crs(r) <- "EPSG:3309"
+# raster::crs(r) <- "EPSG:3309"
+# ext <- extent(-121, -119, 34.43, 40)
+
+# Specify the extent of the subset
+ext <- extent(-50500, 50000, -410000, -290000)
+# ext <- extent(r)
+
+
+# Subset the raster using the extent
+tslf_sub <- crop(tslf, ext)
+gde_sub <- crop(gde, ext)
+
+raster::crs(tslf_sub) <- "EPSG:3309"
+raster::crs(gde_sub) <- "EPSG:3309"
+
+# Create a leaflet map
+leaflet() %>%
+  addTiles() %>%
+  addRasterImage(gde_sub) %>% 
+  addRasterImage(tslf_sub)
+  
+
+  
+  
+pryr::object_size(r)
+
 
 leaflet() %>% 
   addProviderTiles("Esri.WorldImagery") %>%
   setView(-120.829529, 37.538843, zoom = 6) %>%
-  addRasterImage(r) %>% 
+  addRasterImage(r_sub) %>% 
   addLayersControl(overlayGroups = "rasterLayer", options = layersControlOptions(collapsed = FALSE)) %>% 
   addMouseCoordinates() %>% 
   addMeasure(
@@ -30,7 +56,7 @@ leaflet() %>%
     activeColor = "#3D535D",
     completedColor = "#36454F")
 
-
+# subsetting
 
 
  
@@ -39,9 +65,6 @@ leaflet() %>% addTiles() %>%
   addLegend(pal = pal, values = values(r),
             title = "Surface temp")
 
-
-tm_shape(r) +
-  tm_raster()
 
 
 # Create a tmap layer from the raster object
@@ -59,9 +82,9 @@ class(r_layer)
 pal <- colorNumeric(c("#d73027", "#fc8d59", "#fee090", "#e0f3f8", "#91bfdb", "#4575b4"), values(r), na.color = "transparent")
 
 leaflet() %>% 
-  addProviderTiles("Esri.WorldImagery") %>%
-  setView(-120.829529, 37.538843, zoom = 6) %>%
   addRasterImage(r)
+
+# vector memory exhausted - 
 
 
 
