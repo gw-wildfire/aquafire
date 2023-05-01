54fce096b0b869447d3e3cf7796890fac4d9e5f0
Wade Sedgwick <wsedgwick0@gmail.com>
2023-05-01 11:42:56 -0700

    .

--- shiny/dump.R
+++ shiny/dump.R
@@ -13,8 +13,34 @@ sidebarPanel(width = 3,
 # ABOVE AFTER LEAFLET MAP
 
 
+
 tslf <- raster("/Users/Wsedgwick/Desktop/bren_meds/courses/capstone/aquafire/shiny/www/tslf.tif")
 gde <- raster("/Users/Wsedgwick/Desktop/bren_meds/courses/capstone/aquafire/shiny/www/gde_boundaries.tif")
+eco <- st_read("/Users/Wsedgwick/Desktop/bren_meds/courses/capstone/aquafire/data/ca_eco_l3/ca_eco_l3.shp")
+
+# getting coastal mountain ecoregion
+coastal_mountain <- eco[6,]
+
+coastal_mountain <- st_transform(coastal_mountain, crs = st_crs(3309))
+
+coastal_mountain_sf <- st_as_sf(coastal_mountain, crs = st_crs(3309))
+eco_sf <- st_as_sf(eco, crs = st_crs(3309))
+
+
+
+# plot the coastal mountain polygon
+leaflet() %>%
+#  addTiles() %>%
+  addPolygons(data = eco_sf)
+
+
+leaflet() %>% addTiles() %>% 
+  addPolygons(coastal_mountain, lng = -120, lat = 37)
+
+
+
+tm_shape(coastal_mountain) +
+  tm_polygons()
 
 # raster::crs(r) <- "EPSG:3309"
 # ext <- extent(-121, -119, 34.43, 40)
@@ -28,15 +54,24 @@ ext <- extent(-50500, 50000, -410000, -290000)
 tslf_sub <- crop(tslf, ext)
 gde_sub <- crop(gde, ext)
 
+# subsetting by eco-region
+tslf_sub <- crop(tslf, coastal_mountain)
+coastal_mountain
+
 raster::crs(tslf_sub) <- "EPSG:3309"
 raster::crs(gde_sub) <- "EPSG:3309"
 
 # Create a leaflet map
 leaflet() %>%
-  addTiles() %>%
-  addRasterImage(gde_sub) %>% 
+#  addTiles() %>%
+#  addRasterImage(gde_sub) %>% 
   addRasterImage(tslf_sub)
-  
+
+leaflet() %>% 
+  addRasterImage(tslf_sub)
+
+
+plot(tslf_sub)
 
   
   
